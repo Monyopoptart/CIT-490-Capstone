ddfac01f7cda5324aae69b10f3a502956bed6885
Monyopoptart <monyopoptart@gmail.com>
2021-04-25 17:12:53 -0400

    Added a bunch of stuff

---
+++ Source/Crusader/SpawnSomethingWhenOtherThingsDie.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawnSomethingWhenOtherThingsDie.h"
+
+SpawnSomethingWhenOtherThingsDie::SpawnSomethingWhenOtherThingsDie()
+{
+}
+
+SpawnSomethingWhenOtherThingsDie::~SpawnSomethingWhenOtherThingsDie()
+{
+}
